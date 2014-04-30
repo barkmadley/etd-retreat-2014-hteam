ccdbe56a0dc7852ea913565967f00faf3d32292a
Mark Bradley <barkmadley@gmail.com>
2014-05-01 07:45:34 +1000

    small changes
    removed unused bindings and some parameter switching.

--- src/RemosefulProber/Main.hs
+++ src/RemosefulProber/Main.hs
@@ -6,18 +6,17 @@ import System.Random
 import Control.Monad.State
 
 choose :: [(Float, Move)] -> Move -> State StdGen Move
-choose [] d = return Betray
-choose xs d = (\f -> choose' f xs d) `fmap` (state (randomR (0.0,1.0)))
+choose xs d = (choose' xs d) `fmap` (state (randomR (0.0,1.0)))
     where
-        choose' :: Float -> [(Float, a)] -> a -> a
-        choose' p [] d = d
-        choose' p ((pa,a):ps) d
+        choose' :: [(Float, a)] -> a -> Float -> a
+        choose' [] d p = d
+        choose' ((pa,a):ps) d p
             | p < pa = a
             | otherwise = choose' (p - pa) ps d
 
-f ((m,Betray):(Betray,_):(_,Cooperate):rs) = return Cooperate
-f ((m,Betray):rs) = return Betray
-f ((m,Cooperate):rs) =
+f ((_,Betray):(Betray,_):(_,Cooperate):rs) = return Cooperate
+f ((_,Betray):rs) = return Betray
+f ((_,Cooperate):rs) =
     choose [(0.95, Cooperate)] Betray
 f _ = return Cooperate
 
