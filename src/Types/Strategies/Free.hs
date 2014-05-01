8fe7f9af4a792c0f1fc0d432dcdc2fa2ffbb43f9
Mark Bradley <barkmadley@gmail.com>
2014-05-01 17:30:21 +1000

    bit of cleanup.

--- src/Types/Strategies/Free.hs
+++ src/Types/Strategies/Free.hs
@@ -4,6 +4,25 @@ import Data.Monoid
 import Control.Monad.Free
 import qualified System.Random as R
 
+-- Types
+
+data Move
+    = Betray
+    | Cooperate
+    deriving (Show)
+
+charToMove :: Char -> Move
+charToMove 'C' = Cooperate
+charToMove _ = Betray
+
+moveToString :: Move -> String
+moveToString Cooperate = "C"
+moveToString Betray = "D"
+
+type Round = (Move, Move)
+
+-- Strategy actions
+
 data StrategyAction next
     = Set Move next
     | History (Round -> next)
@@ -72,7 +91,6 @@ pchoose ((pa,a):ps) d p
     | p < pa    = a
     | otherwise = pchoose ps d (p-pa)
 
-
 -- Evaluattion
 
 playStratAction :: Move -> [Round] -> Int -> R.StdGen -> Strategy () -> IO ()
@@ -95,82 +113,4 @@ playStrategy s = do
         rounds = zip local opp
     playStratAction Cooperate rounds 1000 rgen s
 
-
-data Move
-    = Betray
-    | Cooperate
-    deriving (Show)
-
-charToMove :: Char -> Move
-charToMove 'C' = Cooperate
-charToMove _ = Betray
-
-moveToString :: Move -> String
-moveToString Cooperate = "C"
-moveToString Betray = "D"
-
---instance Random Move where
---    -- randomR :: RandomGen g => (a, a) -> g -> (a, g)
---    randomR (lo,hi) g =
---            (if rBool then Betray else Cooperate, g')
---        where
---            (rBool, g') = randomR (False,True) g
---    -- random :: RandomGen g => g -> (a, g)
---    random g = randomR (Betray,Cooperate) g
-
-type Round = (Move, Move)
-
---type StratF =
---    [Round] -> State StdGen Move
-
---data Strategy = S (String, StratF)
----- TODO:
----- newtype StrategyT f a = StrategyT ([Round] -> f a)
-
---instance Eq Strategy where
---    S (n1, _) == S (n2, _) = n1 == n2
-
---instance Ord Strategy where
---    compare (S (n1, _)) (S (n2, _)) = compare n1 n2
-
---instance Show Strategy where
---    show (S (n,_)) = n
-
---newtype Score = Score Int
---    deriving (Eq, Ord)
-
---instance Show Score where
---    show (Score i) = show i
-
---instance Monoid Score where
---    mempty = Score 0
---    mappend (Score i) (Score j) = Score (i + j)
-
---type Scores = (Score, Score)
-
---s i = Score i
-
---score :: Move -> Move -> Scores
---score Betray Betray = (s 3, s 3)
---score Betray Cooperate = (s 0,s 5)
---score Cooperate Betray = (s 5,s 0)
---score Cooperate Cooperate = (s 1,s 1)
-
---move :: Strategy -> [Round] -> State StdGen Move
---move (S (_, s)) = s
-
-
---dilemmaMain :: Strategy -> IO ()
---dilemmaMain s = do
---    opponentsline <- getLine
---    localline <- getLine
---    let opp = map charToMove opponentsline
---        local = map charToMove localline
---        rounds = zip local opp
---    gen <- newStdGen
---    let myMove = evalState (move s rounds) gen
---    putStrLn (moveToString myMove)
-
---tftDefault v ((m,t):rs) = return t
---tftDefault v _ = return v
 
