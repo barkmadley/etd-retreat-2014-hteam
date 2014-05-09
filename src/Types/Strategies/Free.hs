36e4eeb2de59f96b81cf8999398abb00998f1aef
Mark Bradley <barkmadley@gmail.com>
2014-05-09 21:17:50 +1000

    finalised an implementation of creating a decision tree from a strategy.

--- src/Types/Strategies/Free.hs
+++ src/Types/Strategies/Free.hs
@@ -1,11 +1,15 @@
 module Types.Strategies.Free where
 
 import Data.Monoid
+import Data.List
 import Data.Maybe
+import Data.Tree
+
 import Control.Monad.Trans.Maybe
 import Control.Monad.Free
 import Control.Monad.RWS
 import Control.Applicative
+
 import qualified System.Random as R
 
 -- Types
@@ -13,7 +17,12 @@ import qualified System.Random as R
 data Move
     = Betray
     | Cooperate
-    deriving (Show, Eq)
+    deriving (Eq, Enum, Ord)
+
+allMoves = [ Betray .. Cooperate ]
+
+instance Show Move where
+    show = moveToString
 
 flipMove Betray = Cooperate
 flipMove Cooperate = Betray
@@ -217,6 +226,86 @@ runFirstStratAction (Rounds f) = ask >>= return . f
 runFirstStratAction (History f) = mzero
 runFirstStratAction (Random f) = state (R.randomR (0.0, 1.0)) >>= return . f
 
+data Hole = Hole
+
+allRounds = [ (a,b) | a <- [Betray .. Cooperate], b <- [Betray .. Cooperate] ]
+
+node a = Node a []
+nodeC a c = Node a c
+
+newtype MMove = M (Maybe Move) deriving (Eq, Ord)
+
+instance Show MMove where
+    show (M Nothing) = "_"
+    show (M (Just x)) = show x
+
+type MRound = (MMove, MMove)
+
+data TreeNode
+    = S Move
+    | H MRound
+    | R Int
+    | Rand Float
+    deriving (Show, Eq, Ord)
+
+instance Ord a => Ord (Tree a) where
+    compare (Node a _) (Node b _) = compare a b
+
+roundToH (a,b) = H (M (Just a), M (Just b))
+
+topLevelNodeValues :: Forest a -> [a]
+topLevelNodeValues = concatMap head . map levels
+
+unH :: TreeNode -> MRound
+unH (H m) = m
+unH _ = error ""
+
+unM (M m) = m
+
+child :: Tree a -> Forest a
+child (Node _ v) = v
+
+eqChildren (Node _ c1) (Node _ c2) = c1 == c2
+
+compareWith f a b = compare (f a) (f b)
+
+unifyTrees :: [Tree TreeNode] -> [Tree TreeNode]
+unifyTrees trees =
+    let
+        mrounds = map unH $ topLevelNodeValues trees
+        oppMoves = map (fromJust . unM . snd) mrounds
+        myMoves = map (fromJust . unM . fst) mrounds
+        joinOppMoves =
+            case nub (sort oppMoves) of
+                [] -> error ""
+                [Betray, Cooperate] -> [Nothing]
+                x -> map Just x
+        joinMyMoves =
+            case nub (sort myMoves) of
+                [] -> error ""
+                [Betray, Cooperate] -> [Nothing]
+                x -> map Just x
+        v = child $ head trees
+    in
+    [ nodeC (H (M a, M b)) v | a <- joinMyMoves, b <- joinOppMoves ]
+
+treeIfyStrategy :: Int -> Int -> [Float] -> Strategy a -> Forest TreeNode
+treeIfyStrategy hdepth n s (Pure _) = []
+treeIfyStrategy hdepth n s (Impure (Set m next)) =
+    [nodeC (S m) (treeIfyStrategy hdepth n s next)]
+treeIfyStrategy hdepth n s (Impure (Rounds f)) =
+    [nodeC (R n) (treeIfyStrategy hdepth n s (f n))]
+treeIfyStrategy 0 n s (Impure (History f)) = []
+treeIfyStrategy hdepth n s (Impure (History f)) =
+    let al = map (\r -> nodeC (roundToH r) (treeIfyStrategy (hdepth - 1) n s (f r))) allRounds
+        g  = groupBy eqChildren $ sortBy (compareWith child) al
+        r = concatMap unifyTrees g
+    in
+    r -- r is the compacted history view
+    --al -- This will give a fully expanded tree
+treeIfyStrategy hdepth n ps (Impure (Random f)) =
+    map (\p -> nodeC (Rand p) (treeIfyStrategy (hdepth - 1) n ps (f p))) ps
+
 -- -- a fully implemented recursive interpreter
 
 playStrategy :: Move -> [Round] -> Int -> R.StdGen -> Strategy a -> IO ()
@@ -271,3 +360,7 @@ runStrategyP1 s = do
         (result,state',lastMove) = runRWS (runMaybeT interpreter) 1000 state
         play = fromMaybe Cooperate (getLast lastMove)
     putStrLn $ moveToString play
+
+printStrategyTree :: Strategy a -> IO ()
+printStrategyTree s = do
+    putStr $ drawForest $ map (fmap show) $ treeIfyStrategy 5 5 [0.0, 1.0] s
